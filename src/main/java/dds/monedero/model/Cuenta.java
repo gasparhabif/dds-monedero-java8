be5cedc80eecdfc43de38b4085c50b2095412bb0
Gaspar Habif <gasparhabif@gmail.com>
2021-05-12 09:28:51 -0300

    Actualizado CodeSmell 6

--- src/main/java/dds/monedero/model/Cuenta.java
+++ src/main/java/dds/monedero/model/Cuenta.java
@@ -29,7 +29,7 @@ public class Cuenta {
   }
 
   private void validarDepositosDiariosPermitidos() {
-    if (getMovimientos().stream().filter(movimiento -> movimiento.isDeposito()).count() >= 3) {
+    if (this.movimientos.stream().filter(Movimiento::isDeposito).count() >= 3) {
       throw new MaximaCantidadDepositosException("Ya excedio los " + 3 + " depositos diarios");
     }
   }
