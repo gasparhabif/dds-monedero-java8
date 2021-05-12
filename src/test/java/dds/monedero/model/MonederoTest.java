ae47de60d1f0fec41acae0240fb818031a3cbd80
Gaspar Habif <gasparhabif@gmail.com>
2021-05-12 09:01:35 -0300

    Arreglados y agregado tests

--- src/test/java/dds/monedero/model/MonederoTest.java
+++ src/test/java/dds/monedero/model/MonederoTest.java
@@ -7,19 +7,26 @@ import dds.monedero.exceptions.SaldoMenorException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class MonederoTest {
   private Cuenta cuenta;
+  private LocalDate hoy;
 
   @BeforeEach
   void init() {
     cuenta = new Cuenta();
+    hoy = LocalDate.now();
   }
 
   @Test
   void Poner() {
-    cuenta.poner(1500);
+    double monto = 1500;
+    cuenta.poner(monto);
+    assertEquals(cuenta.getSaldo(), monto);
   }
 
   @Test
@@ -29,9 +36,17 @@ public class MonederoTest {
 
   @Test
   void TresDepositos() {
-    cuenta.poner(1500);
-    cuenta.poner(456);
-    cuenta.poner(1900);
+    double deposito1 = 1500;
+    double deposito2 = 456;
+    double deposito3 = 1900;
+    double total = deposito1 + deposito2 + deposito3;
+
+    cuenta.poner(deposito1);
+    cuenta.poner(deposito2);
+    cuenta.poner(deposito3);
+
+    assertEquals(cuenta.getSaldo(), total);
+    assertEquals(cuenta.getMovimientos().size(), 3);
   }
 
   @Test
@@ -63,6 +78,36 @@ public class MonederoTest {
   @Test
   public void ExtraerMontoNegativo() {
     assertThrows(MontoNegativoException.class, () -> cuenta.sacar(-500));
+  }
+
+  @Test
+  void ExtraerMontoPermitido(){
+    double saldo = 2500;
+    double monto = 357;
+    double remanente = saldo - monto;
+    cuenta.setSaldo(saldo);
+
+    cuenta.sacar(monto);
+
+    assertEquals(cuenta.getSaldo(), remanente);
+    assertEquals(monto, cuenta.getMontoExtraidoA(hoy));
+    assertEquals(cuenta.getMovimientos().size(), 1);
+  }
+
+  @Test
+  void MovimientoDepositadoHoy() {
+    cuenta.poner(100);
+
+    assert cuenta.getMovimientos().get(0).fueDepositado(hoy);
+  }
+
+  @Test
+  void MovimientoExtraidoHoy() {
+    cuenta.setSaldo(1000);
+
+    cuenta.sacar(100);
+
+    assert cuenta.getMovimientos().get(0).fueExtraido(hoy);
   }
 
 }
